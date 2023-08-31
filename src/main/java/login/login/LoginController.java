6bec51e3067e105c96f5b8a556245e0e76ef02f0
conroy <conroyaw@gmail.com>
2023-08-31 14:51:48 +0100

    added rest template to project

--- src/main/java/login/login/LoginController.java
+++ src/main/java/login/login/LoginController.java
@@ -3,12 +3,14 @@ package login.login;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class LoginController {
 
     @GetMapping("/login/api/vi-manager")
     public ResponseEntity<String> getManagerLogin(){
+        RestTemplate template = new RestTemplate();
         return ResponseEntity.ok("welcome to the login page");
     }
 
